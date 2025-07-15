721d53d0bf98b01b599cd8c4a1852a772fb77824
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-07-15 15:33:13 +0200

    Una specie di menu scarso

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,33 @@
 import * as React from 'react'
+import {
+  Box,
+  Button,
+  ButtonProps,
+  Divider,
+  styled,
+  Typography,
+} from "@mui/material";
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import { red } from "@mui/material/colors";
+
+const RedButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText(red[700]),
+  backgroundColor: red[700],
+  '&:hover': {
+    backgroundColor: red[900],
+  },
+}));
 
 const Header = (): React.ReactElement => {
     return (
-        <span>Header</span>
-    )
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', p: 1, border: '1px solid grey' }}>
+        <RedButton startIcon={<ReceiptIcon />} variant="contained">Nuovo Ordine</RedButton>
+        <Typography sx={{ minWidth: 80 }}>Ordini</Typography>
+        <Typography sx={{ minWidth: 80 }}>Prodotti</Typography>
+        <Divider orientation="vertical" flexItem />
+        <Typography sx={{ minWidth: 80 }}>Admin</Typography>
+      </Box>
+
+        )
 }
 export default Header
