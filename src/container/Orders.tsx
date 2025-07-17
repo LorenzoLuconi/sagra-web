4a7eb233cf536c28a11fefabf60ea58609b597b4
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-07-17 15:01:23 +0200

    Rivisti ordini

--- src/container/Orders.tsx
+++ src/container/Orders.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import {ordersSearchQuery, productByIdQuery} from "../api/sagra/sagraComponents.ts";
-import { convertDate, currency, getQueryObj } from "../utils";
+import { convertDate, currency, getQueryObj, TIME_CONF } from "../utils";
 import {useLocation} from "react-router";
 import {useQuery} from "@tanstack/react-query";
 import {
@@ -19,6 +19,7 @@ import {
 import {Order, OrderedProduct} from "../api/sagra/sagraSchemas.ts";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { EditOutlined, PrintOutlined, SettingsOutlined } from "@mui/icons-material";
 
 interface ProductNameI {
     productId: number
@@ -68,78 +69,111 @@ const OrderRow: React.FC<OrderRowI> = (props) => {
     const {order} = props
     const [open, setOpen] = React.useState(false)
     const createdDate = order.created ? new Date(order.created) : new Date()
-    let total = 0;
-    return (
-        <>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' }, backgroundColor: '#efefef' }}>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell>
-                    {order.customer}
-                </TableCell>
-                <TableCell align="right">{convertDate('it',  createdDate)}</TableCell>
-                <TableCell align="right">{order.note}</TableCell>
-                <TableCell align="right">{order.serviceCost}</TableCell>
-                <TableCell align="right">{order.serviceNumber}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box sx={{ margin: 1 }}>
-                            <Typography variant="h6" gutterBottom component="div">
-                                Prodotti
-                            </Typography>
-                            <Table size="small" aria-label="purchases">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Prodotto</TableCell>
-                                        <TableCell>Quantità</TableCell>
-                                        <TableCell align="right">Costo</TableCell>
-                                        <TableCell align="right">SubTotale (€)</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {order.products &&order.products.map((product:OrderedProduct, idx: number) => {
-                                        const subTotal = product.price * product.quantity
-                                        total += subTotal
-                                        return (
+  return (
+    <>
+      <TableRow
+        sx={{ "& > *": { borderBottom: "unset" }, backgroundColor: "#efefef" }}
+      >
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>
+          {(() => {
+            if ( order.takeAway )
+              return ( <TakeAway /> )
+            else
+              return
+          })()}
+        </TableCell>
+        <TableCell align="center">{order.id}</TableCell>
+        <TableCell align="center">{convertDate("it", createdDate)}</TableCell>
+        <TableCell align="center">{convertDate("it", createdDate, TIME_CONF)}</TableCell>
+        <TableCell>{order.customer}</TableCell>
+        <TableCell align="center">
+          {order.discountRate ? order.discountRate + '%' : ''}
+        </TableCell>
+        <TableCell align="right">{currency(order.totalAmount)}</TableCell>
+        <TableCell align="center">
+            <IconButton disabled><PrintOutlined/></IconButton>
+            <IconButton disabled><EditOutlined/></IconButton>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Prodotti Ordinati
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Prodotto</TableCell>
+                    <TableCell align="center">Quantità</TableCell>
+                    <TableCell align="right">Costo</TableCell>
+                    <TableCell align="right">SubTotale</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  { order.products.map(
+                      (product: OrderedProduct, idx: number) => {
+                        const subTotal = product.price * product.quantity;
+                        return (
+                          <TableRow
+                            key={idx}
+                            sx={{ backgroundColor: "#efefef" }}
+                          >
+                            <TableCell>
+                              <ProductName productId={product.productId} />
+                            </TableCell>
+                            <TableCell align="center">
+                              {product.quantity}
+                            </TableCell>
+                            <TableCell align="right">
+                              {currency(product.price)}
+                            </TableCell>
 
-                                            <TableRow key={idx} sx={{backgroundColor: '#efefef'}}>
-
-
-                                                <TableCell><ProductName productId={product.productId??0}/></TableCell>
-                                                <TableCell>{product.quantity}</TableCell>
-                                                <TableCell align="right">
-                                                    {currency(product.price)}
-                                                </TableCell>
-
-                                                <TableCell align="right">
-                                                    {currency(subTotal)}
-                                                </TableCell>
-                                            </TableRow>
-
-
-                                        )})}
-
-                                    <TableRow>
-                                        <TableCell colSpan={3}><Typography sx={{fontWeight: 700}}>Totale</Typography></TableCell>
-                                        <TableCell align="right"><Typography sx={{fontWeight: 700}}>{currency(total)}</Typography></TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </>
-    )
+                            <TableCell align="right">
+                              {currency(subTotal)}
+                            </TableCell>
+                          </TableRow>
+                        );
+                      },
+                    )}
+                  <TableRow sx={{ backgroundColor: "#efefef" }}>
+                    <TableCell>Coperti</TableCell>
+                    <TableCell align="center">{order.serviceNumber}</TableCell>
+                    <TableCell align="right">
+                      {currency(order.serviceCost / order.serviceNumber)}
+                    </TableCell>
+                    <TableCell align="right">
+                      {currency(order.serviceCost)}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={3}>
+                      <Typography sx={{ fontWeight: 700 }}>Totale</Typography>
+                    </TableCell>
+                    <TableCell align="right">
+                      <Typography sx={{ fontWeight: 700 }}>
+                        {currency(order.totalAmount)}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
 }
 
 
@@ -167,15 +201,18 @@ const Orders = ():React.ReactElement => {
         const orders = ordersData.data
         return (
             <TableContainer component={Paper}>
-                <Table aria-label="collapsible table">
+                <Table stickyHeader aria-label="collapsible table">
                     <TableHead >
                         <TableRow>
-                            <TableCell />
-                            <TableCell>Nome</TableCell>
-                            <TableCell align="right">Creato</TableCell>
-                            <TableCell align="right">Note</TableCell>
-                            <TableCell align="right">Costo Servizio</TableCell>
-                            <TableCell align="right">Numero Servizio</TableCell>
+                            <TableCell/>
+                            <TableCell></TableCell>
+                            <TableCell align="center">#</TableCell>
+                            <TableCell align="center">Data</TableCell>
+                            <TableCell align="center">Ora</TableCell>
+                            <TableCell >Nome</TableCell>
+                            <TableCell align="center">Sconto</TableCell>
+                            <TableCell align="right">Totale</TableCell>
+                            <TableCell align="center"><SettingsOutlined /></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -208,5 +245,11 @@ const Orders = ():React.ReactElement => {
     return (<span>Loading...</span>)
 
 
+}
+
+export const TakeAway = ( props  ) => {
+  return (
+    <img src={'/public/take-away.svg'} alt={'Asporto'} style={{height: '30px', width: 'auto', ...props}}/>
+  )
 }
 export default Orders
