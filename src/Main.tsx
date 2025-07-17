a52ce97f765a6702a86514a005e8ab476963338b
Roberto Ravazzolo <roberto.ravazzolo@iit.cnr.it>
2025-07-17 10:35:35 +0200

    Eliminato warning.

---
+++ src/Main.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {Box} from "@mui/material";
+import {grey} from "@mui/material/colors";
+import App from "./App.tsx";
+import {queryClient} from "./main.tsx";
+
+const Main = (): React.ReactElement => {
+    return (
+
+        <QueryClientProvider client={queryClient}>
+            <Box sx={{padding: 2, paddingRight: 4, paddingLeft: 4, backgroundColor: grey[100], minWidth: '500px'}}>
+                <App />
+            </Box>
+        </QueryClientProvider>
+
+    )
+}
+export default Main
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,15 @@
 import * as React from 'react'
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import {createRoot} from "react-dom/client";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import "./index.css";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Box } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import {QueryClient} from '@tanstack/react-query'
+import Main from "./Main.tsx";
 
 export const queryClient = new QueryClient()
-
-const Main = (): React.ReactElement => {
-    return (
-
-        <QueryClientProvider client={queryClient}>
-          <Box sx={{padding: 2, paddingRight: 4, paddingLeft: 4, backgroundColor: grey[100], minWidth: '500px'}}>
-            <App />
-          </Box>
-        </QueryClientProvider>
-
-    )
-}
 
 
 createRoot(document.getElementById("root")!).render(
