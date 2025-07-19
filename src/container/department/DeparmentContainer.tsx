12e3b74e10f4686e7b81908233e3ec1e389a6227
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-07-19 14:30:48 +0200

    Rimosso box

--- src/container/department/DeparmentContainer.tsx
+++ src/container/department/DeparmentContainer.tsx
@@ -5,18 +5,20 @@ import { WorkspacesOutlined } from "@mui/icons-material";
 
 const DepartmentContainer = () => {
   return (
-    <Box sx={{margin: 2}}>
+    <>
       <Stack direction="row" spacing={1} sx={{mb: 1, alignItems: 'center'}}>
         <WorkspacesOutlined />
         <Typography sx={{fontWeight: 700, fontSize: '1.5em'}}>Reparti</Typography>
       </Stack>
+
       <Paper variant="outlined" sx={{padding: 2}}>
         <DepartmentEdit />
       </Paper>
+
       <Box sx={{mt: 1}}>
         <DepartmentsList />
       </Box>
-    </Box>
+    </>
   );
 };
 
