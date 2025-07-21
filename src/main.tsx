f09a361e0b0d334c7eee9567de778fed9466094d
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-07-21 16:01:19 +0200

    Disattivato retry globalmente

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,15 @@ import "./index.css";
 import {QueryClient} from '@tanstack/react-query'
 import MainComponent from "./MainComponent.tsx";
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      //staleTime: 1000 * 60 * 3,
+    },
+  },
+})
 
 
 createRoot(document.getElementById("root")!).render(
