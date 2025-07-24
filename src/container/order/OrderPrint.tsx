b960e55a67e17f00ab2ac598e9234846ec446fdd
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-07-24 14:34:47 +0200

    Prima bozza per multi pagina

--- src/container/order/OrderPrint.tsx
+++ src/container/order/OrderPrint.tsx
@@ -10,15 +10,27 @@ import {
   Typography
 } from "@mui/material";
 import { Logo } from "../../layout/Logo.tsx";
-import { Order } from "../../api/sagra/sagraSchemas.ts";
+import { Order, OrderedProduct, Product } from "../../api/sagra/sagraSchemas.ts";
 import { currency } from "../../utils";
 import { ProductName } from "../product/ProductName.tsx";
+import "./OrderPrint.css"
+import { productByIdQuery } from "../../api/sagra/sagraComponents.ts";
+import { useQuery } from "@tanstack/react-query";
+import { DepartmentName } from "../department/DepartmentName.tsx";
 
-interface IOrderPrint {
+interface OrderPrintProps {
   order: Order;
 }
 
-const OrderPrint = (props : IOrderPrint ) => {
+interface OrderedProductPrint {
+  productId: number;
+  price: number;
+  quantity: number;
+  department: string
+}
+
+
+const OrderPrint = (props : OrderPrintProps ) => {
 
   const contentRef = useRef<HTMLDivElement>(null);
   const reactToPrintFn = useReactToPrint({ contentRef });
@@ -43,53 +55,42 @@ const OrderPrint = (props : IOrderPrint ) => {
         productId: 2,
         quantity: 3,
         price: 6
+      },
+      {
+        productId: 3,
+        quantity: 2,
+        price: 0.8
       }
     ]
   }
+
+  // La chiave è l'id del prodotto, qualcosa di simile presente in OrderStore
+  const productsMap : Record<number, Product> = {
+    1: { name: 'Tordelli', departmentId: 1 } as Product,
+    2: { name: 'Grigliata Salsicce', departmentId: 2 } as Product,
+    3: { name: 'Panzanell', departmentId: 2 } as Product,
+  }
+
 
 
   return (
     <>
       <button onClick={reactToPrintFn}>Print</button>
-      <Box ref={contentRef} sx={{ p: 5}}>
-        <OrderPrintLogo/>
-        <OrderPrintInfo order={order} />
-        <OrderPrintTitle title="Copia Cliente" />
-        <TableContainer>
-          <Table sx={{width: '100%'}}>
-            <TableHead>
-             <TableRow>
-                <TableCell>Prodotto</TableCell>
-                <TableCell align="center" sx={{ width: '100px'}}>Quantità</TableCell>
-                <TableCell align="right" sx={{ width: '120px'}}>Prezzo Unit.</TableCell>
-                <TableCell align="right" sx={{ width: '120px'}}>Totale</TableCell>
-             </TableRow>
-              { order.products.map(p =>
-                <TableRow key={p.productId} sx={{p: 0}}>
-                  <TableCell><Typography sx={{ fontSize: '1.1em'}}><ProductName productId={p.productId}/></Typography></TableCell>
-                  <TableCell align="center">{p.quantity}</TableCell>
-                  <TableCell align="right">{currency(p.price)}</TableCell>
-                  <TableCell align="right">{currency(p.price * p.quantity)}</TableCell>
-                </TableRow>
-              )}
+      <div ref={contentRef} className="printContent print-container">
+        <OrderPrintPageCustomer order={order} />
 
-              { order.serviceNumber > 0 ?
-                <TableRow >
-                  <TableCell><Typography sx={{ fontSize: '1.1em'}}>Coperti</Typography></TableCell>
-                  <TableCell align="center">{order.serviceNumber}</TableCell>
-                  <TableCell align="right">{currency(order.serviceCost)}</TableCell>
-                  <TableCell align="right">{currency(order.serviceNumber * order.serviceCost)}</TableCell>
-                </TableRow>
-                : ''
-              }
-                <TableRow sx={{ display: 'none'}} >
-                  <TableCell colSpan={3} align="right" sx={{ fontSize: '1.1em', fontWeight: 500 }}>TOTALE</TableCell>
-                  <TableCell colSpan={3} align="right" sx={{ fontSize: '1.1em', fontWeight: 500 }}>{currency(order.totalAmount)}</TableCell>
-                </TableRow>
-            </TableHead>
-          </Table>
-        </TableContainer>
-      </Box>
+        {
+          Array.from(new Set(Object.values(productsMap).map(product => product.departmentId)).values()).map((departmentId: number) => {
+            return (
+              <>
+                <div className="page-break" />
+                <OrderPrintPageDepartment key={departmentId} order={order} departmentId={+departmentId} productsMap={productsMap} />
+              </>
+            )
+          })
+        }
+
+      </div>
     </>
   )
 }
@@ -111,6 +112,114 @@ const FieldValue = (props: IFieldValue) => {
       </Box>
     </Box>
   )
+}
+
+interface OrderPrintPageCustomerProps {
+  order: Order;
+}
+
+const OrderPrintPageCustomer =  (props: OrderPrintPageCustomerProps) => {
+  const {order} = props;
+
+  return (
+    <>
+      <Box sx={{ m: 3}}>
+        <OrderPrintLogo/>
+        <OrderPrintInfo order={order} />
+        <OrderPrintTitle title="Copia Cliente" />
+        <TableContainer>
+          <Table sx={{width: '100%'}}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Prodotto</TableCell>
+                <TableCell align="center" sx={{ width: '100px'}}>Quantità</TableCell>
+                <TableCell align="right" sx={{ width: '120px'}}>Prezzo Unit.</TableCell>
+                <TableCell align="right" sx={{ width: '120px'}}>Totale</TableCell>
+              </TableRow>
+            </TableHead>
+              { order.products.map(p =>
+                <TableRow key={p.productId} sx={{p: 0}}>
+                  <TableCell><Typography sx={{ fontSize: '1.1em'}}><ProductName productId={p.productId}/></Typography></TableCell>
+                  <TableCell align="center">{p.quantity}</TableCell>
+                  <TableCell align="right">{currency(p.price)}</TableCell>
+                  <TableCell align="right">{currency(p.price * p.quantity)}</TableCell>
+                </TableRow>
+              )}
+
+              { order.serviceNumber > 0 ?
+                <TableRow >
+                  <TableCell><Typography sx={{ fontSize: '1.1em'}}>Coperti</Typography></TableCell>
+                  <TableCell align="center">{order.serviceNumber}</TableCell>
+                  <TableCell align="right">{currency(order.serviceCost)}</TableCell>
+                  <TableCell align="right">{currency(order.serviceNumber * order.serviceCost)}</TableCell>
+                </TableRow>
+                : ''
+              }
+              <TableRow sx={{ display: 'none'}} >
+                <TableCell colSpan={3} align="right" sx={{ fontSize: '1.1em', fontWeight: 500 }}>TOTALE</TableCell>
+                <TableCell colSpan={3} align="right" sx={{ fontSize: '1.1em', fontWeight: 500 }}>{currency(order.totalAmount)}</TableCell>
+              </TableRow>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  )
+
+}
+
+interface OrderPrintPageDepartmentProps {
+  order: Order;
+  departmentId: number;
+  productsMap: Record<number, Product>;
+}
+
+const OrderPrintPageDepartment =  (props: OrderPrintPageDepartmentProps) => {
+  const {order, departmentId, productsMap} = props;
+
+  const productForDepartment = () => {
+    const result : OrderedProduct[] = [];
+    for ( const op of order.products) {
+      const productsMapElement = productsMap[op.productId];
+      if (productsMapElement && productsMapElement.departmentId == departmentId) {
+        result.push(op)
+      }
+    }
+
+    return result;
+  }
+
+  const productsToPrint = productForDepartment()
+
+  return (
+    <>
+      <Box sx={{ m: 3}}>
+        <OrderPrintLogo/>
+        <OrderPrintInfo order={order} />
+        <Box sx={{ mt: 5, textAlign: 'center' }}>
+          <Typography sx={{fontSize: '1.8em', color: 'text.primary', textTransform: 'uppercase'}}>
+            <DepartmentName departmentId={departmentId}/>
+          </Typography>
+        </Box>
+        <TableContainer>
+          <Table sx={{width: '100%'}}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Prodotto</TableCell>
+                <TableCell align="center" sx={{ width: '100px'}}>Quantità</TableCell>
+              </TableRow>
+            </TableHead>
+              { productsToPrint.map(p =>
+                <TableRow key={p.productId} sx={{p: 0}}>
+                  <TableCell><Typography sx={{ fontSize: '1.1em'}}><ProductName productId={p.productId}/></Typography></TableCell>
+                  <TableCell align="center">{p.quantity}</TableCell>
+                </TableRow>
+              )}
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  )
+
 }
 
 const OrderPrintLogo = () => {
@@ -167,5 +276,31 @@ const OrderPrintTitle = (props: OrderPrintTitleProps) => {
     </Box>
   )
 }
+
+interface ProductMapProps {
+  orderedProduct: OrderedProduct
+}
+const ProductMap = (props: ProductMapProps) => {
+  const {orderedProduct} = props
+
+  const productConf = productByIdQuery({
+    pathParams: { productId: orderedProduct.productId }
+  });
+
+  const productData = useQuery({
+    queryKey: productConf.queryKey,
+    queryFn: productConf.queryFn,
+  });
+
+  if (productData.isLoading) {
+    return <></>;
+  }
+
+  if (productData.isError) {
+    return <>Error</>;
+  }
+}
+
+
 
 export default OrderPrint;
