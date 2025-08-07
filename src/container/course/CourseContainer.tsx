b4356f6d2e8482e47e9450979e352a63beafdb18
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-08-07 10:59:08 +0200

    Aggiunti un po' di pulsanti di refresh

--- src/container/course/CourseContainer.tsx
+++ src/container/course/CourseContainer.tsx
@@ -1,19 +1,41 @@
-import {Paper, useTheme} from "@mui/material";
-import {FormatListNumberedOutlined} from "@mui/icons-material";
+import {Box, IconButton, Paper, useTheme} from "@mui/material";
+import {CachedOutlined, FormatListNumberedOutlined} from "@mui/icons-material";
 import {CourseEdit} from "./CourseEdit.tsx";
 import CoursesList from "./CoursesList.tsx";
 import PageTitle from "../../view/PageTitle.tsx";
+import { coursesSearchQuery } from "../../api/sagra/sagraComponents.ts";
+import {queryClient} from "../../main.tsx";
+import toast from "react-hot-toast";
 
 const CourseContainer = () => {
     const theme = useTheme();
+
+    const searchConf = coursesSearchQuery({});
+    const handleRefresh = () => {
+        queryClient.invalidateQueries({queryKey: searchConf.queryKey}).then(() => {
+            toast.success("Elenco portate aggiornato", {duration: 2000})
+        }).catch((e: Error) => {console.log('Errore: ', e)})
+    }
+
 
     return (
         <>
             <PageTitle title="Portate" icon={<FormatListNumberedOutlined/>}/>
             <Paper variant="outlined"
-                   sx={{padding: 2, backgroundColor: theme.sagra.panelBackground}}
+                   sx={{
+                       display: "flex",
+                       justifyContent: "space-between",
+                       padding: 2,
+                       flexWrap: "wrap",
+                       gap: 1,
+                       backgroundColor: theme.sagra.panelBackground}}
                    className='paper-top'>
                 <CourseEdit/>
+                <Box>
+                    <IconButton sx={{ width: '40px'}}>
+                        <CachedOutlined onClick={handleRefresh} />
+                    </IconButton>
+                </Box>
             </Paper>
             <Paper variant="outlined"
                    sx={{padding: 2, backgroundColor: theme.sagra.panelBackground}}
--- src/container/department/DeparmentContainer.tsx
+++ src/container/department/DeparmentContainer.tsx
@@ -1,19 +1,39 @@
-import {Paper, useTheme} from "@mui/material";
+import {IconButton, Paper, useTheme} from "@mui/material";
 import {DepartmentEdit} from "./DepartmentEdit";
 import DepartmentsList from "./DepartmentsList.tsx";
-import {WorkspacesOutlined} from "@mui/icons-material";
+import {CachedOutlined, WorkspacesOutlined} from "@mui/icons-material";
 import PageTitle from "../../view/PageTitle.tsx";
+import {departmentsSearchQuery} from "../../api/sagra/sagraComponents.ts";
+import {queryClient} from "../../main.tsx";
+import toast from "react-hot-toast";
 
 const DepartmentContainer = () => {
     const theme = useTheme();
+
+    const searchConf = departmentsSearchQuery({});
+    const handleRefresh = () => {
+        queryClient.invalidateQueries({queryKey: searchConf.queryKey}).then(() => {
+            toast.success("Elenco reparti aggiornato", {duration: 2000})
+        }).catch((e: Error) => {console.log('Errore: ', e)})
+    }
 
     return (
         <>
             <PageTitle title="Reparti" icon={<WorkspacesOutlined/>}/>
 
-            <Paper variant="outlined" sx={{padding: 2, backgroundColor: theme.sagra.panelBackground}}
+            <Paper variant="outlined"
+                   sx={{
+                       display: "flex",
+                       justifyContent: "space-between",
+                       padding: 2,
+                       flexWrap: "wrap",
+                       gap: 1,
+                       backgroundColor: theme.sagra.panelBackground}}
                    className="paper-top">
                 <DepartmentEdit/>
+                <IconButton sx={{ width: '40px'}}>
+                    <CachedOutlined onClick={handleRefresh} />
+                </IconButton>
             </Paper>
 
             <Paper variant="outlined" sx={{mt: 1, p: 2, backgroundColor: theme.sagra.panelBackground}}
