883417694c7552d21d3fe161fc2ba24bf2b1a8d3
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-08-08 14:33:28 +0200

    Aggiunto calcolatore del resto. Riorganizzato OrderEditContainer

--- src/container/order/OrderEditContainer.tsx
+++ src/container/order/OrderEditContainer.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useQueries } from "@tanstack/react-query";
 import { productByIdQuery } from "../../api/sagra/sagraComponents.ts";
 import {Box, CircularProgress, Grid, Paper, Typography, useTheme} from "@mui/material";
-import { OrderStore } from "../../context/OrderStore.tsx";
+import {OrderStore, useOrderStore} from "../../context/OrderStore.tsx";
 import ProductsToOrder from "./ProductsToOrder.tsx";
 import ErrorInfo from "../../view/ErrorInfo.tsx";
 import OrderEditTotal from "./OrderEditTotal.tsx";
@@ -10,82 +10,116 @@ import OrderEditProducts from "./OrderEditProducts.tsx";
 import OrderEditForm from "./OrderEditForm.tsx";
 import { Order } from "../../api/sagra/sagraSchemas.ts";
 import {convertDate, TIME_CONF} from "../../utils";
+import OrderEditRest from "./OrderEditRest.tsx";
+import {isEqual} from "lodash";
 
 interface OrderEditContainerProps {
   order: Order;
 }
 
- const OrderEditContainer: React.FC<OrderEditContainerProps> = (props) => {
-  const { order } = props;
-  const theme = useTheme();
+const OrderEditContainer :React.FC<OrderEditContainerProps> = (props: OrderEditContainerProps) => {
+    const { order } = props;
 
-  const combinedQueries = useQueries({
-    queries: order.products.map((product) => {
-      const productConf = productByIdQuery({
-        pathParams: { productId: product.productId }
-      });
-      return {
-        queryKey: productConf.queryKey,
-        queryFn: productConf.queryFn
-      };
-    }),
-    combine: (results) => {
-      return {
-        data: results.map((result) => result.data),
-        pending: results.some((result) => result.isPending)
-      };
+    const combinedQueries = useQueries({
+        queries: order.products.map((product) => {
+            const productConf = productByIdQuery({
+                pathParams: { productId: product.productId }
+            });
+            return {
+                queryKey: productConf.queryKey,
+                queryFn: productConf.queryFn
+            };
+        }),
+        combine: (results) => {
+            return {
+                data: results.map((result) => result.data),
+                pending: results.some((result) => result.isPending)
+            };
+        }
+    });
+
+    if (combinedQueries.pending) {
+        return <CircularProgress />;
     }
-  });
 
-  if (combinedQueries.pending) {
-    return <CircularProgress />;
-  }
-  if (combinedQueries.data ) {
+    if ( ! combinedQueries.data)
+        return <></>
+
     return (
-      <OrderStore products={combinedQueries.data} order={order}>
+        <OrderStore products={combinedQueries.data} order={order}>
+            <OrderEditInnerContainer />
+        </OrderStore>
+    )
+}
+
+const OrderEditInnerContainer: React.FC = () => {
+    const theme = useTheme();
+    const {order, originalOrder, isNewOrder} = useOrderStore();
+
+    const isOrderChanged = !isEqual(originalOrder, order)
+
+    const showCalc = () => {
+        return !isNewOrder()  && ! isOrderChanged && order.totalAmount > 0
+    }
+
+    return (
         <Grid container spacing={2}>
-          <Grid size={7}>
-            <ProductsToOrder />
-          </Grid>
-          <Grid size={5}>
-              <Box sx={{display: 'flex', flexDirection: 'column', position: 'sticky', top: '10px'}}>
-            <ErrorInfo />
+            <Grid size={7}>
+                <ProductsToOrder/>
+            </Grid>
+            <Grid size={5} sx={{minWidth: '400px'}}>
+                <Box sx={{display: 'flex', flexDirection: 'column', position: 'sticky', top: '10px'}}>
+                    <ErrorInfo/>
 
-            {
-              order.id > 0 &&
-              <Paper variant="outlined"
-                     sx={{ p: 0.5, textAlign: 'center', backgroundColor: theme.sagra.panelBackground, filter: 'brightness(85%)' }}
-                     className="paper-top">
-                <Typography sx={{fontWeight: 500, fontSize: '1.1em'}}>Ordine n. {order.id} del {convertDate('it', new Date(order.created))} ore {convertDate('it', new Date(order.created), TIME_CONF)}</Typography>
-              </Paper>
-            }
+                    {
+                        ! isNewOrder() &&
+                        <Paper variant="outlined"
+                               sx={{
+                                   p: 0.5,
+                                   textAlign: 'center',
+                                   backgroundColor: theme.sagra.panelBackground,
+                                   filter: 'brightness(85%)'
+                               }}
+                               className="paper-top">
+                            <Typography sx={{fontWeight: 500, fontSize: '1.1em'}}>Ordine
+                                n. {order.id} del {convertDate('it', new Date(order.created))} ore {convertDate('it', new Date(order.created), TIME_CONF)}</Typography>
+                        </Paper>
+                    }
 
-            <Paper variant="outlined"
-                   sx={{ p: 0.5, backgroundColor: theme.sagra.panelBackground }}
-                   className="paper-middle">
-              <OrderEditTotal />
-            </Paper>
+                    <Paper variant="outlined"
+                           sx={{p: 0.5, backgroundColor: theme.sagra.panelBackground}}
+                           className="paper-middle">
+                        <OrderEditTotal/>
+                    </Paper>
 
-            <Paper variant="outlined"
-                   sx={{ p: 0.5, backgroundColor: theme.sagra.panelBackground }}
-                   className="paper-middle">
-              <OrderEditProducts />
-            </Paper>
+                    <Paper variant="outlined"
+                           sx={{p: 0.5, backgroundColor: theme.sagra.panelBackground}}
+                           className="paper-middle">
+                        <OrderEditProducts/>
+                    </Paper>
 
-            <Paper
-              variant="outlined"
-              sx={{ padding: 2, backgroundColor: theme.sagra.panelBackground }}
-              className="paper-bottom"
-            >
-              <OrderEditForm />
-            </Paper>
-              </Box>
-          </Grid>
+                    <Paper
+                        variant="outlined"
+                        sx={{padding: 2, backgroundColor: theme.sagra.panelBackground}}
+                        className={ isNewOrder() || isOrderChanged ? 'paper-bottom' : 'paper-middle'}
+                    >
+                        <OrderEditForm/>
+                    </Paper>
+
+                    { showCalc() &&
+                        <Paper
+                            variant="outlined"
+                            sx={{padding: 2, backgroundColor: theme.sagra.panelBackground}}
+                            className="paper-bottom"
+                        >
+                            <OrderEditRest/>
+                        </Paper>
+                    }
+                </Box>
+            </Grid>
 
         </Grid>
-      </OrderStore>
-    );
-  }
-};
+    )
+}
 
 export default OrderEditContainer;
---
+++ src/container/order/OrderEditRest.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import {currency} from "../../utils";
+import {Box, TextField, Typography} from "@mui/material";
+import {useOrderStore} from "../../context/OrderStore.tsx";
+import { CalculateOutlined } from "@mui/icons-material";
+
+
+const OrderEditRest: React.FC = () => {
+    const {order} = useOrderStore();
+    const {totalAmount} = order
+
+    const [cash, setCash] = React.useState<number | undefined>(undefined);
+    const [rest, setRest] = React.useState<number | undefined>(undefined);
+
+    const handleChangeCash =
+        React.useCallback<React.ChangeEventHandler<HTMLInputElement>>((event) => {
+                const value = Number.parseFloat(event.target.value)
+                if (!isNaN(value)) {
+                    setCash(value);
+
+                    if (value && value > totalAmount)
+                        setRest(value - totalAmount);
+                    else
+                        setRest(undefined);
+                } else {
+                    setCash(undefined);
+                }
+            }, [setCash, setRest, totalAmount],
+        );
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if ( event.key == 'Escape' ) {
+            setCash(undefined)
+            setRest(undefined)
+        }
+    };
+
+    return (
+        <Box sx={{display: 'flex', justifyContent: "flex-start", alignItems: "center", gap: 2}}>
+            <Box sx={{ width: '50px' }}>
+                <CalculateOutlined sx={{ fontSize: '2rem' }}/>
+            </Box>
+            <TextField
+                size="small"
+                variant="outlined"
+                type="number"
+                value={cash ?? ''}
+                placeholder="es: 88.50"
+                onChange={handleChangeCash}
+                label="Contanti"
+                onKeyUp={handleKeyPress}
+                slotProps={{ htmlInput: { size: 8, min: 1 } }}
+            />
+
+            { rest &&
+                <Box sx={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center', minWidth: '150px'}}>
+                    <Typography component="div" sx={{fontSize: '0.9rem', textTransform: 'uppercase' }}>Resto: </Typography>
+                    <Typography component="div" sx={{ml: 1, fontSize: '1.2rem', minWidth: '150px'}}>{currency(rest)}</Typography>
+                </Box>
+            }
+        </Box>
+    )
+}
+
+export default OrderEditRest;
