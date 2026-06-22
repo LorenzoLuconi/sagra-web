f2e677e7b28cc7b2b063d31ff0d443fdfd9cdc2c
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2026-06-22 11:44:49 +0200

    Aggiornate vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,46 +7,56 @@ export default defineConfig({
     port: 3000, // change here
   },
   build: {
-    rollupOptions: {
+    rolldownOptions: {
       output: {
-        manualChunks(id) {
-          if (!id.includes("node_modules")) {
-            return;
-          }
-
-          if (id.includes("/react/") || id.includes("/react-dom/") || id.includes("/react-router")) {
-            return "vendor-react";
-          }
-
-          if (id.includes("/@emotion/")) {
-            return "vendor-emotion";
-          }
-
-          if (id.includes("/@mui/icons-material/")) {
-            return "vendor-mui-icons";
-          }
-
-          if (id.includes("/@mui/x-date-pickers/")) {
-            return "vendor-mui-date-pickers";
-          }
-
-          if (id.includes("/@mui/x-charts/")) {
-            return "vendor-mui-charts";
-          }
-
-          if (id.includes("/@mui/material/") || id.includes("/@mui/system/") || id.includes("/@mui/utils/")) {
-            return "vendor-mui";
-          }
-
-          if (id.includes("/@tanstack/react-query/")) {
-            return "vendor-query";
-          }
-
-          if (id.includes("/dayjs/")) {
-            return "vendor-dayjs";
-          }
-
-          return "vendor";
+        codeSplitting: {
+          groups: [
+            {
+              name: "vendor-react",
+              test: /node_modules[\\/](react|react-dom|react-router)([\\/]|$)/,
+              priority: 90,
+            },
+            {
+              name: "vendor-emotion",
+              test: /node_modules[\\/]@emotion[\\/]/,
+              priority: 80,
+            },
+            {
+              name: "vendor-mui-icons",
+              test: /node_modules[\\/]@mui[\\/]icons-material[\\/]/,
+              priority: 70,
+            },
+            {
+              name: "vendor-mui-date-pickers",
+              test: /node_modules[\\/]@mui[\\/]x-date-pickers[\\/]/,
+              priority: 70,
+            },
+            {
+              name: "vendor-mui-charts",
+              test: /node_modules[\\/]@mui[\\/]x-charts[\\/]/,
+              priority: 70,
+            },
+            {
+              name: "vendor-mui",
+              test: /node_modules[\\/]@mui[\\/](material|system|utils)[\\/]/,
+              priority: 60,
+            },
+            {
+              name: "vendor-query",
+              test: /node_modules[\\/]@tanstack[\\/]react-query[\\/]/,
+              priority: 50,
+            },
+            {
+              name: "vendor-dayjs",
+              test: /node_modules[\\/]dayjs[\\/]/,
+              priority: 50,
+            },
+            {
+              name: "vendor",
+              test: /node_modules[\\/]/,
+              priority: 10,
+            },
+          ],
         },
       },
     },
